62f5e4ce40431397efbfddd6ff2f6f2058367feb
Konstantin Böhm <konstantin@launchco.com>
2016-10-14 10:48:33 +0200

    added action to string state

--- js/lib/ai/dbLayer/stateToKeyString.js
+++ js/lib/ai/dbLayer/stateToKeyString.js
@@ -1,12 +1,12 @@
-export default function (stateAction) {
+export default function stateToKeyString(stateAction) {
   let grid = stateAction.state; // 2D grid array
   let result = '';
-  for(var i = 0 , len = grid.length ; i < len ; i++){
-    for(var j = grid[i].length - 1 ; j > -1 ; j--){
+  for(let i = 0 , len = grid.length ; i < len ; i++){
+    for(let j = grid[i].length - 1 ; j > -1 ; j--){
       let value = grid[i][j];
       result += value;
       if (value === 0) break;
     }
   }
-  return result;
+  return result + '.' + stateAction.action;
 }
