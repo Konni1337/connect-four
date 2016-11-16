7d946411775e3eac3f3fe01ec68e17c066bd014d
Konstantin Böhm <konstantin@launchco.com>
2016-11-16 12:42:31 +0100

    removed unused constants

--- js/constants/GameFixtures.js
+++ js/constants/GameFixtures.js
@@ -1,17 +1,11 @@
-export const UCT_FACTOR = 1 / Math.sqrt(2);
 export const DRAW = 'draw';
 export const HUMAN = 'Human Player';
 export const MCTS = 'Monte Carlo Tree Search';
 export const Q_LEARNING = 'QLearning';
 export const RANDOM = 'Random';
-export const PLAYER_TYPES = [Q_LEARNING, MCTS, HUMAN];
-export const PLAYER_TYPES_FOR_TRAINING = [Q_LEARNING, MCTS];
 export const GRID_LENGTH = 7;
 export const GRID_HEIGHT = 6;
 export const EMPTY_VALUE = 0;
-export const GAME_TYPE_NORMAL = 'normal';
-export const GAME_TYPE_TRAINING = 'training';
-export const GAME_TYPE_NONE = 'none';
 
 export const Q_LEARNING_CONFIG = {
   DEFAULT_REWARDS: {
